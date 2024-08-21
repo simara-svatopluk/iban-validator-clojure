348f254061651ca14b35bc868ab712961f381e25
Svaťa Šimara <svatopluk.simara@gmail.com>
2024-08-21 13:01:05 +0200

    Add Clojure project structure

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject iban-validator-clojure "0.1.0-SNAPSHOT"
+  :description "Demonstration that Clojure is actually useful"
+  :url "https://github.com/simara-svatopluk/iban-validator-clojure"
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns iban-validator-clojure.core})
