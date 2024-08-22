364fcd49c2da7a953ca045ad2861d474a2aad80f
Svaťa Šimara <svatopluk.simara@gmail.com>
2024-08-22 21:06:58 +0200

    Update ring

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject iban-validator-clojure "0.1.0-SNAPSHOT"
   :description "Demonstration that Clojure is actually useful"
   :url "https://github.com/simara-svatopluk/iban-validator-clojure"
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.5"]]
+  :dependencies [
+                 [org.clojure/clojure "1.11.1"]
+                 [compojure "1.7.1"]
+                 [ring/ring-core "1.12.1"]
+                 [ring/ring-jetty-adapter "1.12.1"]
+                 [ring/ring-defaults "0.5.0"]
+                 ]
+  :plugins [[lein-ring "0.12.6"]]
   :ring {:handler iban-validator-clojure.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
